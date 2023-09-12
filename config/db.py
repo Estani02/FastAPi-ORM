64ca28f0f03a947f233c294ab7cb839e9b68f88a
Estani02 <estaniolmedo@gmail.com>
2023-09-12 15:36:17 -0300

    edit variable env

--- config/db.py
+++ config/db.py
@@ -2,8 +2,7 @@ from decouple import config
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
 
-# Añade ?sslmode=require a la URL de conexión
-engine = create_engine('postgresql://estani:mplfypreha0O0h8ohKfAV3FVQ4VhyoGW@dpg-ck08fnb6fquc73d2f7sg-a/uppercase_convert')
+engine = create_engine(config("DATABASE_URL"))
 
 meta = MetaData()
 
