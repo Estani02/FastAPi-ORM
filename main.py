a0619b03f8b86df22b7f2df5c656a3175010101e
Estani02 <estaniolmedo@gmail.com>
2023-09-13 13:55:46 -0300

    watermark

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from service import read_all_data, read_data, create_data, InputData, GetData
 
 app = FastAPI()
 
+@app.get('/')
+def welcome():
+    return 'Api desarrollada por Estanislao Olmedo! :)'
+
 @app.get('/get_data')
 def get_users(skip: int = 0, limit: int = 100):
     return read_all_data(skip, limit)
